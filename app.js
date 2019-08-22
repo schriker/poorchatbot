b9054ce69934ce7e37f6b7541e0a7be4f708ed4c
schriker <janekmachine@gmail.com>
2019-08-22 18:08:42 +0200

    Mongo db localhost

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const spinner = ora({
   spinner: 'line'
 })
 
+const mongoHost = process.env.NODE_ENV === 'development' 
+  ? `mongodb+srv://${config.DB_USERNAME}:${config.DB_PASS}@cluster0-vsmqj.mongodb.net/${config.DB_NAME}?retryWrites=true` 
+  : `mongodb://127.0.0.1:27017/${config.DB_NAME}`
+  
 spinner.start()
 mongoose.connect(`mongodb+srv://${config.DB_USERNAME}:${config.DB_PASS}@cluster0-vsmqj.mongodb.net/${config.DB_NAME}?retryWrites=true`, {
   useNewUrlParser: true,
