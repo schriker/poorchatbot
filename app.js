fb6df06e363a176dfa53ed0a7bd26622e47ba776
schriker <janekmachine@gmail.com>
2019-09-07 20:39:19 +0200

    Base for wykop notifier bot

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bot = require('./bot')
+const Wykop = require('./wykop')
 const config = require('./config.json')
 
 const mongoHost = `mongodb://${config.DB_USERNAME}:${config.DB_PASS}@3.121.239.54/${config.DB_NAME}`
@@ -9,9 +10,30 @@ mongoose.connect(mongoHost, {
   useNewUrlParser: true,
   useFindAndModify: false
   })
-  .then(() => {
+  .then(async () => {
     console.log('DB connected!')
-    bot()
+    // bot()
+    // const wykopNotifier = new Wykop({
+    //   secret: config.WYKOP.SECRET,
+    //   appKey: config.WYKOP.APPKEY
+    // })
+    // await wykopNotifier.request({
+    //   requestMethod: 'POST',
+    //   apiParams: ['login', 'index'],
+    //   namedParams: null,
+    //   postParams: {
+    //     accountkey: config.WYKOP.ACCOUNTKEY
+    //   }
+    // })
+    // wykopNotifier.request({
+    //   requestMethod: 'POST',
+    //   apiParams: ['entries', 'add'],
+    //   namedParams: null,
+    //   postParams: {
+    //     body: 'testowa wiadomosc bota'
+    //   }
+    // })
+    // .then(res => console.log(res)) 
   })
   .catch(err => {
     console.log(err)
