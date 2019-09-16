97fd51efb847a2e96207c65154358def46913dae
schriker <ubuntu@ip-172-31-36-172.eu-central-1.compute.internal>
2019-09-16 16:45:46 +0000

    Remove after stream notification

--- bot.js
+++ bot.js
@@ -115,7 +115,7 @@ const bot = async () => {
                 const video = new FacebookVideo(facebookVideoData)
                 await video.save()
                 console.log(`FB Vide Saved - ${facebookVideoData.title}`)
-                client.say(`https://jarchiwum.pl/wonziu/${videoData.videoID}?platform=facebook`)
+		//setTimeout(() => client.say(`https://jarchiwum.pl/wonziu/${videoData.videoID}?platform=facebook`), 300000)
                 isFacebook = false
             } catch (error) {
                 console.log(error)
