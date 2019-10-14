6cfd96f51d9bb992554e41835c83bfdb584bb32b
schriker <janekmachine@gmail.com>
2019-10-14 21:21:15 +0200

    Unique video name on downloading

--- facebookVideoDownloader.js
+++ facebookVideoDownloader.js
@@ -6,9 +6,9 @@ const FacebookVideo = require('./models/facebookVideo')
 const facebookVideoDownloader = (video) => {
     let comand
     if (video.public === true) {
-      comand = `youtube-dl https://www.facebook.com/StrumienieZRuczaju/videos/${video.facebookId}/ -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]" -o "video.%(ext)s"`
+      comand = `youtube-dl https://www.facebook.com/StrumienieZRuczaju/videos/${video.facebookId}/ -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]" -o "${video.facebookId}.%(ext)s"`
     } else if (video.public === false) {
-      comand = `youtube-dl ${video.url} -f "best[ext=mp4]" -o "video.%(ext)s"`
+      comand = `youtube-dl ${video.url} -f "best[ext=mp4]" -o "${video.facebookId}%(ext)s"`
     }
     exec(comand, 
     async (error, stdout, stderr) => {
@@ -16,8 +16,8 @@ const facebookVideoDownloader = (video) => {
           console.error(`exec error: ${error}`)
           return
         }
-      const youTubeVideo = await youtubeUpload('video.mp4', video)
-      fs.unlinkSync('video.mp4')
+      const youTubeVideo = await youtubeUpload(`${video.facebookId}.mp4`, video)
+      fs.unlinkSync(`${video.facebookId}.mp4`)
       const videoInDatabase = await FacebookVideo.findById(video._id)
       videoInDatabase.youTubeId = youTubeVideo.data.id
       videoInDatabase.thumbnail = youTubeVideo.data.snippet.thumbnails.medium.url
