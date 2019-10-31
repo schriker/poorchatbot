878bfd5f506a6f575b300716fc2ea95bb18bcacf
schriker <janekmachine@gmail.com>
2019-10-31 11:18:36 +0100

    Messages per minute

--- bot.js
+++ bot.js
@@ -167,10 +167,10 @@ const bot = async () => {
         const messages = await Message.find({ createdAt: { $gt: video.started, $lt: video.createdAt } }).sort({ createdAt: 'asc' })
         const duration = new Date(video.createdAt) - new Date(video.started) // ms
 
-        for (let i = 0; i < duration; i += 20000 ) {
+        for (let i = 0; i < duration; i += 60000 ) {
           const messagesCount = messages.filter((message) => {
             const messageTime = new Date(message.createdAt) - new Date(video.started)
-            if (messageTime > i && messageTime < i + 20000) {
+            if (messageTime > i && messageTime < i + 60000) {
               return true
             } else {
               return false
--- t.js
+++ t.js
@@ -45,10 +45,10 @@ const chart = () => {
             const messages = await Message.find({ createdAt: { $gt: video.started, $lt: video.createdAt } }).sort({ createdAt: 'asc' })
             const duration = new Date(video.createdAt) - new Date(video.started) // ms
   
-            for (let i = 0; i < duration; i += 20000 ) {
+            for (let i = 0; i < duration; i += 60000 ) {
               const messagesCount = messages.filter((message) => {
                 const messageTime = new Date(message.createdAt) - new Date(video.started)
-                if (messageTime > i && messageTime < i + 20000) {
+                if (messageTime > i && messageTime < i + 60000) {
                   return true
                 } else {
                   return false
