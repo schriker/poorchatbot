18d23ac02169316bbf543138b35889389e1b9d63
schriker <janekmachine@gmail.com>
2019-10-31 22:35:41 +0100

    Facebook credentials for downloading

--- facebookVideoDownloader.js
+++ facebookVideoDownloader.js
@@ -3,11 +3,12 @@ const moment = require('moment')
 const { exec } = require('child_process')
 const youtubeUpload = require('./youtubeUpload')
 const FacebookVideo = require('./models/facebookVideo')
+const config = require('./config.json')
 
 const facebookVideoDownloader = (video) => {
     let comand
     if (video.public === true) {
-      comand = `youtube-dl https://www.facebook.com/StrumienieZRuczaju/videos/${video.facebookId}/ -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]" -o "${video.facebookId}.%(ext)s"`
+      comand = `youtube-dl https://www.facebook.com/StrumienieZRuczaju/videos/${video.facebookId}/ -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]" --username ${config.FB.LOGIN} --password ${config.FB.PASSWORD} -o "${video.facebookId}.%(ext)s"`
     } else if (video.public === false) {
       comand = `youtube-dl ${video.url} -f "best[ext=mp4]" -o "${video.facebookId}.%(ext)s"`
     }
