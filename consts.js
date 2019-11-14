5898cbee78f144b2039bfeb3028af4126117bfd3
schriker <janekmachine@gmail.com>
2019-11-14 22:52:48 +0100

    Lucek added

--- consts.js
+++ consts.js
@@ -33,16 +33,22 @@ exports.streamers = [
     twitchId: '22386881',
     highLights: highLights
   },
+  // {
+  //   name: 'leh',
+  //   pageId: '1477765125857920',
+  //   twitchId: '101879951',
+  //   highLights: highLights
+  // },
+  // {
+  //   name: 'nexos',
+  //   pageId: '137607326365861',
+  //   twitchId: '31298665',
+  //   highLights: highLights
+  // },
   {
-    name: 'leh',
-    pageId: '1477765125857920',
-    twitchId: '101879951',
+    name: 'luceklive',
+    pageId: '285928328182715',
+    twitchId: '31889910',
     highLights: highLights
   },
-  {
-    name: 'nexos',
-    pageId: '137607326365861',
-    twitchId: '31298665',
-    highLights: highLights
-  }
 ]
