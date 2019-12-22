9cabf6e9ca97afc898abe5bad8a607e296c2b06b
schriker <janekmachine@gmail.com>
2019-12-22 14:01:42 +0100

    No DB Connection for tests only

--- app.js
+++ app.js
@@ -4,25 +4,27 @@ const bot = require('./bot')
 const wykopNotifier = require('./wykopNotifier')
 const CronJob = require('cron').CronJob
 
-const mongoHost = `mongodb://${config.DB_USERNAME}:${config.DB_PASS}@${config.DB_HOST}/${config.DB_NAME}`
+bot()
 
-console.log('Contecting to DB...')
-mongoose.connect(mongoHost, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-  })
-  .then(async () => {
-    console.log('DB connected!')
-    bot()
-    try {      
-      new CronJob('00 00 04 * * *', () => {
-      	wykopNotifier()
-      }, null, true)
-    } catch (cronerr) {
-      console.log('Invalid cron')
-    }
-  })
-  .catch(err => {
-    console.log(err)
-  })
+// const mongoHost = `mongodb://${config.DB_USERNAME}:${config.DB_PASS}@${config.DB_HOST}/${config.DB_NAME}`
+
+// console.log('Contecting to DB...')
+// mongoose.connect(mongoHost, {
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+//   useUnifiedTopology: true
+//   })
+//   .then(async () => {
+//     console.log('DB connected!')
+//     bot()
+//     try {      
+//       new CronJob('00 00 04 * * *', () => {
+//       	wykopNotifier()
+//       }, null, true)
+//     } catch (cronerr) {
+//       console.log('Invalid cron')
+//     }
+//   })
+//   .catch(err => {
+//     console.log(err)
+//   })
